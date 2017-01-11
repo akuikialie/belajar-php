bfb8cb5dacf65349c32e960328259047f4b65eb0
Rendy Ananta <rendy.ananta66@gmail.com>
2017-01-11 10:41:52 +0700

    Add tipe data boolean

---
+++ 02_tipe_data/04_boolean/index.php
@@ -0,0 +1,19 @@
+<?php
+
+/** Tipe Data Boolean
+  * Ini tipe data dengan value tersedikit
+  * Value nya hanya true atau false
+  * jadi, kalau nggak true ya false
+  * kalau nggak benar ya salah
+  * kalau nggak salah ya benar :v
+  *
+  * Contoh :
+  */
+
+  $a = true;
+  var_dump($a);
+  // Akan menghasilkan keluaran :
+  // bool(true)
+
+  // disitu terlihat tipe datanya adalah bool yaitu singkatan boolean
+  // isi variabelnya true
