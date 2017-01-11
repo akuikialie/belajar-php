b76777e16f6fec41ddf7568c8cfea6da52068af3
Rendy Ananta <rendy.ananta66@gmail.com>
2017-01-11 10:38:53 +0700

    Add tipe data float

---
+++ 02_tipe_data/03_float/index.php
@@ -0,0 +1,17 @@
+<?php
+
+/** Tipe Data Float
+  * Berisilan data bilangan desimal yang tidak penuh
+  * maksudnya pecahan hehe :v
+  * misalkan 0.1, 2.4 itu termasuk tipe data float
+  *
+  * Contoh :
+  */
+
+  $a = 0.75;
+  var_dump($a);
+  // Akan menghasilkan keluaran :
+  // float(0.75)
+
+  // disitu terlihat tipe datanya adalah float dan
+  // isi variabelnya 0.75
