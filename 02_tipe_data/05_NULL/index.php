f47334d552e0a10050e922f1f1acad1efe8b151f
Rendy Ananta <rendy.ananta66@gmail.com>
2017-01-11 10:53:37 +0700

    Add tipe data NULL

---
+++ 02_tipe_data/05_NULL/index.php
@@ -0,0 +1,20 @@
+<?php
+
+/** Tipe Data NULL
+  * Ini tipe data yang nggak ada valuenya :v
+  * Jadi tipe data NULL merupakan spesial tipe data yang hanya dapat diisi value NULL
+  * Sebuah variabel yang dibuat tanpa value, maka secara otomatis akan terisi NULL
+  * variabel juga bisa dikosongkan dengan memberinya value NULL
+  * $a = NULL;
+  * ^ dengan cara tersebut variabel menjadi kosong valuenya
+  *
+  * Contoh :
+  */
+
+  $a;
+  var_dump($a);
+  // Akan menghasilkan keluaran :
+  // NULL
+
+  // ya gitu deh, NULL
+  // cuman NULL
