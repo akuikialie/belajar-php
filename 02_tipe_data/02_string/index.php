97de55530f6643de06366100172f01ec4ca6a6e6
Rendy Ananta <rendy.ananta66@gmail.com>
2017-01-11 10:34:24 +0700

    Add tipe data string

---
+++ 02_tipe_data/02_string/index.php
@@ -0,0 +1,20 @@
+<?php
+
+/** Tipe Data String
+  * berisi gabungan dari berbagai karakter, contohnya : 'aku'
+  * tipe data string dalam penulisannya harus diawali dan diakhiri
+  * dengan single quote(') atau double quote(")
+  * di dalam quotes bisa berisi teks didalamnya
+  *
+  * Contoh :
+  */
+
+  $a = "Halo Gan !!!";
+  var_dump($a);
+  // Akan menghasilkan keluaran :
+  // string(12) "Halo Gan !!!"
+
+  // disitu tertulis string(12) "Halo Gan !!!"
+  // terlihat tipe datanya adalah string,
+  // banyak karakternya 12, dan
+  // isi variabelnya adalah "Halo Gan !!!"
